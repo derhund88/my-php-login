3873f5851e3b4fe29a78f4ff070a120a3e63381f
Derhund88 <76412692+derhund88@users.noreply.github.com>
2025-06-26 23:33:21 +0700

    Delete index.php

--- index.php
+++
@@ -1,6 +0,0 @@
-<!-- index.php -->
-<form action="auth.php" method="post">
-  Username: <input type="text" name="username"><br>
-  Password: <input type="password" name="password"><br>
-  <input type="submit" value="Login">
-</form>
